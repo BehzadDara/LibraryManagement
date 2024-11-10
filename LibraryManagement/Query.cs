a0649299bea1a3926b61c6da5b7f202c15e4c185
behzad dara <Behzad.dara.99@gmail.com>
2024-11-10 15:42:20 +0330

    filter added

--- LibraryManagement/Query.cs
+++ LibraryManagement/Query.cs
@@ -7,5 +7,15 @@ public class Query
 {
     public IQueryable<Book> GetBooks([Service] LibraryManagementDBContext context) => context.Books.Include(b => b.Author);
     public IQueryable<Author> GetAuthors([Service] LibraryManagementDBContext context) => context.Authors.Include(a => a.Books);
-    public IQueryable<User> GetUsers([Service] LibraryManagementDBContext context) => context.Users.Include(u => u.BorrowedBooks).ThenInclude(b => b.Author);
+    public IQueryable<User> GetUsers(string? q, [Service] LibraryManagementDBContext context)
+    {
+        var query = context.Users.Include(u => u.BorrowedBooks).ThenInclude(b => b.Author).AsQueryable();
+
+        if (!string.IsNullOrEmpty(q))
+        {
+            query = query.Where(u => u.Name.Contains(q));
+        }
+
+        return query;
+    }
 }
